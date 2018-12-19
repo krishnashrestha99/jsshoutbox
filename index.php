7e96ea5e89719f13082596885275839338ded045
Krishna Shrestha <krishna@shrestha.com>
2018-12-19 18:37:29 +0545

    minor update

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>JS Shoutbox</title>
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="js/script.js"></script>
+</head>
+<body>
+
+</body>
+</html>
